8d599e3a3313a245e2f663fccac0958dfb6f573b
hsisco <hysensisco@gmail.com>
2020-08-07 11:36:21 -0400

    ex1 MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,11 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    num_indices = {}
+    for (i, wt) in enumerate(weights):
+        complement = limit - wt
+        if complement in num_indices:
+            complement_i = num_indices[complement]
+            return (i, complement_i)
+        else:
+            num_indices[wt] = i
 
     return None
