0153ec3dbc2fccb07e48109836ee00172ad4c61c
hsisco <hysensisco@gmail.com>
2020-08-07 11:53:08 -0400

    ex2 MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,11 +1,11 @@
 def get_indices_of_item_weights(weights, length, limit):
-    num_indices = {}
-    for (i, wt) in enumerate(weights):
-        complement = limit - wt
-        if complement in num_indices:
-            complement_i = num_indices[complement]
-            return (i, complement_i)
-        else:
-            num_indices[wt] = i
+    hash_table = {}                             # Create a dictionary
 
-    return None
+    for i in range(length):                     # Iterate through the array
+        difference = limit - weights[i]         # Find the weight limit
+        if difference in hash_table:            # Check if a key with the difference exists
+            return (i, hash_table[difference])  # If YES return the matching items in a tuple
+        else:
+            hash_table[weights[i]] = i          # If NO add it to hash table, with value of index
+
+    return None                                 # If it doesn't exist return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,18 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    hash_table = {}                                                 # Create a dictionary
+
+    route = [None] * length                                         # Create array with None as key value pairs
+
+    for i in range(length):                                         # Iterate through array
+        hash_table[tickets[i].source] = tickets[i].destination      # Add the destinations to hash table 
+        if tickets[i].source == "NONE" and route[0] == None:        # If the current ticket index has a source of None ...
+            route[0] = tickets[i].destination                       # ... this is the first flight
+
+    # Check the value of the starting location to construct the entire route 
+    for ticket in range(length):                            # Iterate through array
+        if ticket > 0 and route[ticket - 1] is not None:    # If the ticket isn't the first AND the last index is not None ...
+            route[ticket] = hash_table[route[ticket - 1]]   # ... Set destination to the ticket
 
     return route
